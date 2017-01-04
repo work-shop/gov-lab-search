0ac28d341b1b6d4b8ae083ddf0a01181c69547d1
Nic Schumann <nicolas_schumann@brown.edu>
2017-01-04 15:47:19 -0500

    [Styling] enriched and standardized rendering for search results

--- document-render.js
+++ document-render.js
@@ -3,8 +3,9 @@
 var moment = require('moment');
 
 /**
- *
- *
+ * This module deals with rendering the results of a search query.
+ * It's job is to take a partition of the search results, divided by
+ * type, and render these results in the search window.
  *
  */
 module.exports = function( search ) {
@@ -13,15 +14,19 @@ module.exports = function( search ) {
     var queryResultCount = $('#result-count');
     var cutoff = 2;
 
+    /**
+     * This object contains header rendering methods divided by type
+     *
+     */
     var renderHeader = {
         "projects": function(  ) {
-            return $('<li>').attr('class', 'mt4 mb2').append( $('<h3>').attr('class', "results-header header-bar small uppercase bold").text( "Projects" ) );
+            return $('<li>').attr('class', 'invisible col-xs-12 mt4 mb2').append( $('<h3>').attr('class', "results-header  header-bar small uppercase bold").text( "Projects" ) );
         },
         "news": function(  ) {
-            return $('<li>').attr('class', 'mt4 mb2').append($('<h3>').attr('class', "results-header header-bar small uppercase bold").text( "News" ) );
+            return $('<li>').attr('class', 'invisible col-xs-12 mt4 mb2').append($('<h3>').attr('class', "results-header  header-bar small uppercase bold").text( "News" ) );
         },
         "output": function(  ) {
-            return $('<li>').attr('class', 'mt4 mb2').append( $('<h3>').attr('class', "results-header header-bar small uppercase bold").text( "Research" ) );
+            return $('<li>').attr('class', 'invisible col-xs-12 mt4 mb2').append( $('<h3>').attr('class', "results-header  header-bar small uppercase bold").text( "Research" ) );
         }
     };
 
@@ -29,43 +34,47 @@ module.exports = function( search ) {
 
         "projects": function( result ) {
             if ( typeof result.image !== "undefined" ) {
-                return $('<li>').attr('class', "row project-result search-result mb2")
+                return $('<li>').attr('class', "invisible col-xs-12 project-result search-result mb3")
                        .append( $('<a>').attr('href', result.slug).attr('class', 'link')
                                 .append( $('<div>').attr('class', 'col-sm-3').append($('<img>').attr('class','bio-image').attr('src', [result.image,'=w300-h200'].join('') ))
                                 )
                                 .append( $('<div>').attr('class', 'col-sm-9')
-                                         .append( $('<h4>').attr('class', "bold").text( result.title ) )
+                                         .append( $('<h4>').attr('class', "bold mt0").text( result.title ) )
                                          .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.summary ) )
                                 )
                         );
 
             } else {
-                return $('<li>').attr('class', "p0 row project-result search-result mb2")
+                return $('<li>').attr('class', "invisible p0 col-xs-12 project-result search-result mb2")
                        .append( $('<a>').attr('href', result.slug).attr('class', 'link')
                                 .append( $('<div>').attr('class', 'col-sm-12')
-                                         .append( $('<h4>').attr('class', "bold").text( result.title ) )
+                                         .append( $('<h4>').attr('class', "bold mt0").text( result.title ) )
                                          .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.summary ) )
                                 )
                         );
             }
         },
         "news": function( result ) {
-            return $('<li>').attr('class', "p0 row project-result search-result mb2")
+            return $('<li>').attr('class', "invisible p0 col-xs-12 project-result search-result mb2")
                    .append( $('<a>').attr('href', result.slug).attr('class', 'link')
                             .append( $('<div>').attr('class', 'col-sm-12')
-                                     .append( $('<h5>').attr('class', "brand uppercase").text( moment( result.date ).format('MMMM Y') ) )
-                                     .append( $('<h4>').attr('class', "bold").text( result.title ) )
+                                     .append( $('<h6>').attr('class', "bold uppercase").text( moment( result.date ).format('MMMM Y') ) )
+                                     .append( $('<h4>').attr('class', "bold mt0").text( result.title ) )
                                      .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.summary ) )
+                                     .append( $('<div>').attr('class', "block news-rule-xs mt2 mb1") )
+                                     .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.author ) )
                             )
                     );
         },
         "output": function( result ) {
-            return $('<li>').attr('class', "p0 row project-result search-result mb2")
+            return $('<li>').attr('class', "invisible p0 col-xs-12 project-result search-result mb2")
                    .append( $('<a>').attr('href', result.slug).attr('class', 'link')
                             .append( $('<div>').attr('class', 'col-sm-12')
-                                     .append( $('<h5>').attr('class', "brand uppercase").text( result.outputType ) )
+                                     .append( $('<h6>').attr('class', "output-type bold brand mt0").text( result.outputType ) )
                                      .append( $('<h4>').attr('class', "bold").text( result.title ) )
-                                     .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.summary ) )
+                                //   .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.summary ) )
+                                     .append( $('<div>').attr('class', "block news-rule-xs mt2 mb1") )
+                                     .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.authors.join(', ') ) )
                             )
                     );
         }
@@ -80,7 +89,7 @@ module.exports = function( search ) {
                 return (count === 1) ? "Update" : "Updates";
 
             case "output":
-                return (count === 1) ? "Research Product" : "Research Products";
+                return (count === 1) ? "Research Result" : "Research Results";
 
             default:
                 throw new Error('Search: Unrecognized Result Type (' + type + ').' );
@@ -96,20 +105,27 @@ module.exports = function( search ) {
             if ( typeof results[ type ] !== "undefined" ) {
                 var resultCount = results[ type ].length;
 
-                queryResultCount.find( ['.', type, '-count' ].join('') ).addClass('bold').text( resultCount );
+                queryResultCount.find( ['.', type, '-count' ].join('') ).addClass('bold').addClass('brand').text( resultCount );
                 queryResultCount.find( ['.', type, '-postfix' ].join('') ).addClass('bold').text( describe( resultCount, type ) );
 
                 results[ type ].forEach( function( result, i ) {
 
-                    if ( i === 0 ) { queryOutput.append( renderHeader[ type ]() ); }
+                    if ( i === 0 ) {
+                        var header = renderHeader[ type ]()
+                        queryOutput.append( header );
+                        header.removeClass('invisible');
+                    }
 
-                    queryOutput.append( render[ type ]( result ) );
+                    var renderedResult = render[ type ]( result );
+
+                    queryOutput.append( renderedResult );
+                    renderedResult.removeClass('invisible');
 
                 });
 
             } else {
 
-                queryResultCount.find( ['.', type, '-count' ].join('') ).removeClass('bold').text( 0 );
+                queryResultCount.find( ['.', type, '-count' ].join('') ).removeClass('bold').removeClass('brand').text( 0 );
                 queryResultCount.find( ['.', type, '-postfix' ].join('') ).removeClass('bold').text( describe( 0, type ) );
 
             }
