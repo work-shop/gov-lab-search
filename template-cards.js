619c4bbc3a3b19e6bd16e4917d1bbe252c76f3d0
Nic Schumann <nicolas_schumann@brown.edu>
2017-02-06 16:43:47 -0500

    [Connectivity] modified the thumbnails to be square-cropped.

--- template-cards.js
+++ template-cards.js
@@ -4,13 +4,27 @@ var moment = require('moment');
 
 module.exports = {
 
+    // <li class="p0 col-sm-offset-1 col-sm-10 project-result search-result mb3">
+    //     <a href="{{ url( item ) }}" class="link">
+    //         <div class="col-sm-2 hidden-xs related-image-block-container">
+    //             <div class="related-image-block" style="background-image:url('{{ item.images[0]|imageSize(250) }}');"></div>
+    //         </div>
+    //         <div class="col-sm-10 col-xs-12">
+    //             <span class="h6 output-type uppercase bold mt0">Project</span>
+    //             <span class="h6 uppercase gray bold mt0">{{ item.publish_date|date('F Y') }}</span>
+    //             <h4 class="bold mt0">{{ item.name }}</h4>
+    //             <p class="project-description small hidden-xs">{{ item.summary }}</p>
+    //         </div>
+    //     </a>
+    // </li>
+
+
     "projects": function( result ) {
         if ( typeof result.image !== "undefined" ) {
             return $('<li>').attr('class', "invisible p0 col-xs-12 project-result search-result mb3")
                    .append( $('<a>').attr('href', result.slug).attr('class', 'link')
-                            .append( $('<div>').attr('class', 'col-sm-2')
-
-                                .append($('<img>').attr('class','bio-image').attr('src', [result.image,'=w300-h200'].join('') ))
+                            .append( $('<div>').attr('class', 'col-sm-2 hidden-x related-image-block-container')
+                                .append( $('<div>').attr('class', 'related-image-block').attr('style', ['background-image:url(\'', [result.image,'=w300-h200'].join(''), '\');'].join('') ) )
                             )
                             .append( $('<div>').attr('class', 'col-sm-10')
                                      .append( $('<span>').attr('class', "h6 uppercase output-type bold mt0").text( "Project" ) )
@@ -38,11 +52,9 @@ module.exports = {
         if ( typeof result.image !== "undefined" ) {
             return $('<li>').attr('class', "invisible p0 col-xs-12 project-result search-result mb3")
                 .append( $('<a>').attr('href', result.slug).attr('class', 'link')
-                        .append( $('<div>').attr('class', 'col-sm-2 hidden-xs')
-                            .append( $('<div>').attr('class', 'search-cover-image').append(
-                                $('<img>').attr('class','bio-image').attr('src', [result.image,'=w300-h200'].join('') )
-                            )
-                        ))
+                        .append( $('<div>').attr('class', 'col-sm-2 hidden-x related-image-block-container')
+                            .append( $('<div>').attr('class', 'related-image-block').attr('style', ['background-image:url(\'', [result.image,'=w300-h200'].join(''), '\');'].join('') ) )
+                        )
                         .append( $('<div>').attr('class', 'col-sm-10 col-xs-12')
                                  .append( $('<span>').attr('class', "h6 output-type uppercase bold mt0").text( "Update" ) )
                                  .append( $('<span>').attr('class', "h6 gray bold uppercase mt0").text( ', ' + moment( result.date ).format('MMMM Y') ) )
@@ -71,15 +83,13 @@ module.exports = {
         if ( typeof result.image !== "undefined" ) {
             return $('<li>').attr('class', "invisible p0 col-xs-12 project-result search-result mb3")
                    .append( $('<a>').attr('href', result.slug).attr('class', 'link')
-                               .append( $('<div>').attr('class', 'col-sm-2 hidden-xs')
-                                   .append( $('<div>').attr('class', 'search-cover-image').append(
-                                       $('<img>').attr('class','').attr('src', [result.image,'=w300-h200'].join('') )
-                                   )
-                               ))
+                           .append( $('<div>').attr('class', 'col-sm-2 hidden-x related-image-block-container')
+                               .append( $('<div>').attr('class', 'related-image-block').attr('style', ['background-image:url(\'', [result.image,'=w300-h200'].join(''), '\');'].join('') ) )
+                           )
 
                             .append( $('<div>').attr('class', 'col-sm-10 col-xs-12')
-                            .append( $('<span>').attr('class', "h6 output-type uppercase bold mt0").text( result.outputType ) )
-                            .append( $('<span>').attr('class', "h6 gray bold uppercase mt0").text( ', ' + moment( result.date ).format('MMMM Y') ) )
+                                     .append( $('<span>').attr('class', "h6 output-type uppercase bold mt0").text( result.outputType ) )
+                                     .append( $('<span>').attr('class', "h6 gray bold uppercase mt0").text( ', ' + moment( result.date ).format('MMMM Y') ) )
                                      .append( $('<h4>').attr('class', "bold mt0").text( result.title ) )
                                      .append( $('<p>').attr('class', "project-description small hidden-xs").text( result.summary ) )
                                     // .append( $('<div>').attr('class', "block news-rule-xs mt2 mb1") )
